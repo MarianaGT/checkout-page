4ad7ca7c8a3fd0dbb3eaa8da5f29d89e63989f51
Mariana Galan <gatm870125@gmail.com>
2021-04-26 19:04:51 -0500

    Form: validation - JS variables

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-const phoneValid = /^[0-9]/
-const emailValid= /^([\w\.\-_]+)?\w+@[\w-_]+(\.\w+){1,}$/igm
+const phoneValid = /(\(?\+?\d{1,3}\)?[\s-]+)?\(?\d{1,3}\)?[\s-]+\d{3}[\s-]?\d{2}[\s-]?\d{2}/g
+const emailValid= /a/
