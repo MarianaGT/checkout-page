c62b15e1616a1047137ce01f047dcd1685c8bae0
Mariana Galan <gatm870125@gmail.com>
2021-04-26 19:12:14 -0500

    Form: validation - JS variables #2

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 const phoneValid = /(\(?\+?\d{1,3}\)?[\s-]+)?\(?\d{1,3}\)?[\s-]+\d{3}[\s-]?\d{2}[\s-]?\d{2}/g
 const emailValid= /a/
+
+const email = document.getElementById("mail");
+
+email.addEventListener("input", function (event) {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("I am expecting an e-mail address!");
+  } else {
+    email.setCustomValidity("");
+  }
+});
