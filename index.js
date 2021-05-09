2c777ee7bd3a698bf8ed2c822d55dc6ba5b72f39
Mariana Galan <gatm870125@gmail.com>
2021-05-09 12:11:16 -0500

    validations js solution #4

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const phoneValid = /(\(?\+?\d{1,3}\)?[\s-]+)?\(?\d{1,3}\)?[\s-]+\d{3}[\s-]?\d{2}[\s-]?\d{2}/g;
-const emailValid= /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
+const emailValid = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 function ValidateEmail() {
-  var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  if (document.emailform.mail.value.match(validRegex)) {
-    document.getElementById("errormsg").innerHTML="Valid email address!";
-    return true;
-  } else {
-    document.getElementById("errormsg").innerHTML="Invalid email address!";
-    alert("Invalid email address!");
-    return false;
+  var email = document.getElementById("mail")
+  var txt = "";
+  if (email.validityState.patternMismatch) {
+    txt = "Hey there";
   }
+  document.getElementById("errormsg").innerHTML= txt;
 }
